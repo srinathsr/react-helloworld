e4890c78c2255a5517d400360028f9ffdb549887
srinathsr <srsrinath97@gmail.com>
2019-11-20 20:38:10 +0530

    Updates

---
+++ precache-manifest.6bec8abab510d8d39d8bbf88f6511174.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5289a623e8777e98344468c6befd5f9a",
+    "url": "/react-helloworld/index.html"
+  },
+  {
+    "revision": "429f13b3123739b6994a",
+    "url": "/react-helloworld/static/js/2.84e780f3.chunk.js"
+  },
+  {
+    "revision": "52078e88fbfad6fe93be",
+    "url": "/react-helloworld/static/js/main.0d34e11f.chunk.js"
+  },
+  {
+    "revision": "8a31cf5284011a0b2ca7",
+    "url": "/react-helloworld/static/js/runtime-main.4760f19f.js"
+  }
+]);
